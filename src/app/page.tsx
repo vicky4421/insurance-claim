14cdaba3d4c5aad55a3d463ea4c50a36ac515c60
vicky <vicky210490@gmail.com>
2024-11-07 23:53:56 +0530

    define home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <main>Login page</main>
+    <main className="min-h-screen flex flex-col items-center justify-between p-24">
+      <div className="z-10    text-xl lg:flex">
+        Welcome to the Insurance Company
+      </div>
+    </main>
   );
 }
