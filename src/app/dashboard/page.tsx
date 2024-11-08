a09bed177c368b44c7800379e7bae869b0886031
vicky <vicky210490@gmail.com>
2024-11-08 17:05:03 +0530

    define dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,21 @@
+'use client'
+import { redirect } from "next/navigation";
+
 export default function Dashboard() {
-    return <div>Dashboard</div>;
+    return (
+        <div className="mt-36">
+            <div className="text-3xl cursor-pointer font-semibold max-w-[600px] border border-[#1971c2] mx-auto text-[#1971c2] rounded-lg p-4 m-4 text-center hover:text-white hover:bg-[#1971c2]">
+                Motor
+            </div>
+            <div className="text-3xl cursor-pointer font-semibold max-w-[600px] border border-[#1971c2] mx-auto text-[#1971c2] rounded-lg p-4 m-4 text-center hover:text-white hover:bg-[#1971c2]">
+                Health
+            </div>
+            <div className="text-3xl cursor-pointer font-semibold max-w-[600px] border border-[#1971c2] mx-auto text-[#1971c2] rounded-lg p-4 m-4 text-center hover:text-white hover:bg-[#1971c2]">
+                Travel
+            </div>
+            <div onClick={() => redirect("/claim-form")} className="text-3xl cursor-pointer font-semibold max-w-[600px] border border-[#1971c2] mx-auto text-[#1971c2] rounded-lg p-4 m-4 text-center hover:text-white hover:bg-[#1971c2]">
+                Claim
+            </div>
+        </div>
+    )
 }
