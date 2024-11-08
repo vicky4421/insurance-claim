82639f0b769f5c0f815efc73001217d126d1a56f
vicky <vicky210490@gmail.com>
2024-11-08 17:05:54 +0530

    reduce height of main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-between p-24">
-      <div className="z-10    text-xl lg:flex">
+    <main className="min-h-[90vh] flex flex-col items-center justify-between p-24">
+      <div className="z-10 text-xl lg:flex">
         Welcome to the Insurance Company
       </div>
     </main>
