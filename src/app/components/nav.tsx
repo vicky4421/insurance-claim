24de4691b2e4b277a9bdcd4e543899819be4d963
vicky <vicky210490@gmail.com>
2024-11-08 17:03:53 +0530

    add dashboard link to nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -7,14 +7,13 @@ export default function Nav() {
 
     const { isLoggedIn, setIsLoggedIn } = useAuth();
 
-    console.log('nav', isLoggedIn)
-
     return (
         <nav className="bg-[#1971c2] p-4 flex items-center justify-between text-white px-10">
             <div className="font-bold">
                 <Link href="/">Insurance Company    </Link>
             </div>
-            <div>
+            <div className="flex gap-4">
+                {isLoggedIn && <Link href="/dashboard">Dashboard</Link>}
                 {
                     isLoggedIn
                         ? <Link href="/login" onClick={() => { setIsLoggedIn(false) }}>Logout</Link>
